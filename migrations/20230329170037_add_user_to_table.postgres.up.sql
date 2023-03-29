777d8232b2f09404db07a9f67d36b33e75049047
andres15mol <andres15mol@gmail.com>
2023-03-29 11:11:55 -0600

    ff
    ff

---
+++ migrations/20230329170037_add_user_to_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.users
+(first_name, last_name, email, "password", acces_level, created_at, updated_at)
+VALUES('Andres', 'Molina', 'admin@admin.com', '$2a$12$TXT5ia4t8HGAR/rOottYbutGTwTtsxOJBJFJvWRX2wZIYpnbHcjqS', 3, '2023-02-08 00:00:00.000', '2023-02-08 00:00:00.000');
+
